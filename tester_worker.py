38cb4241abc4ff9a10c28a31511cb6de42ff1215
t-proffitt <tep.87@outlook.com>
2026-03-05 14:02:50 -0800

    Update tester_worker.py

--- tester_worker.py
+++ tester_worker.py
@@ -3,7 +3,7 @@ from worker import add_numbers
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add_numbers(10, 5), 15)
+        self.assertEqual(add_numbers(10, 5), 16)
     
     def test_add_negative(self):
         self.assertEqual(add_numbers(-1, 1), 0)
