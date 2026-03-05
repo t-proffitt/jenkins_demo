7a1cc02ec66a0a8fccaf9290efed9c2d115ec618
t-proffitt <tep.87@outlook.com>
2026-03-05 14:31:23 -0800

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,11 +1,9 @@
 def add_numbers(a, b):
     return a + b
 
-# We define the numbers here so the test script can "see" them
 NUM_1 = 13
 NUM_2 = 2
 
 if __name__ == "__main__":
-    # We use the variables here instead of hardcoding the numbers
     result = add_numbers(NUM_1, NUM_2)
     print(f"The sum of {NUM_1} and {NUM_2} is: {result}")
