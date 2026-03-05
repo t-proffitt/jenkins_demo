e7fa37eb5a27de10d6c6178f1675908a39f88ba7
t-proffitt <tep.87@outlook.com>
2026-03-05 12:28:59 -0800

    Create tester_worker.py

---
+++ tester_worker.py
@@ -0,0 +1,12 @@
+import unittest
+from worker import add_numbers
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add_numbers(10, 5), 15)
+    
+    def test_add_negative(self):
+        self.assertEqual(add_numbers(-1, 1), 0)
+
+if __name__ == "__main__":
+    unittest.main()
