7a54ef1db5fb2ecf080f114ee449f940d1c14c28
t-proffitt <tep.87@outlook.com>
2026-03-05 14:30:30 -0800

    Update tester_worker.py

--- tester_worker.py
+++ tester_worker.py
@@ -1,12 +1,12 @@
 import unittest
-# Now we import the function AND the specific variables
+#import the function & specific variables
 from worker import add_numbers, NUM_1, NUM_2
 
 class TestWorker(unittest.TestCase):
     def test_dynamic_numbers(self):
-        # The test automatically uses whatever numbers you set in worker.py.
-        # It adds them together and expects the answer to ALWAYS be 15.
+        #test automatically uses whatever numbers are set in worker.py
         self.assertEqual(add_numbers(NUM_1, NUM_2), 15)
+        #sum of NUM_1 & NUM_2 should =15
 
 if __name__ == "__main__":
     unittest.main()
