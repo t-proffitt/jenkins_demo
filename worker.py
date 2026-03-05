ab40386f300680717429cf27d232163a061d1fca
t-proffitt <tep.87@outlook.com>
2026-03-05 14:06:10 -0800

    Update worker.py

--- worker.py
+++ worker.py
@@ -2,5 +2,5 @@ def add_numbers(a, b):
     return a + b
 
 if __name__ == "__main__":
-    result = add_numbers(10, 5)
+    result = add_numbers(10, 12)
     print(f"The sum of 10 and 5 is: {result}")
